ec673cb3541187b06ee3a39afe2eb6a82d758124
touredri <dcompte223@gmail.com>
2023-03-29 21:40:26 +0100

    Maked updateDate arrow function

--- modules/date.js
+++ modules/date.js
@@ -1,11 +1,10 @@
 import { DateTime } from './luxon.js';
 
-export default function updateDate() {
-  const dateTime = DateTime.now();
-  const customFormat = dateTime.toFormat('MMMM dd, yyyy');
+const updateDate = () => {
   // display the formatted date in an HTML element
   const element = document.querySelector('.date');
   if (element !== null) {
-    element.textContent = customFormat;
+    element.textContent = DateTime.now().toFormat('MMMM dd, yyyy');
   }
-}
+};
+export default updateDate;
